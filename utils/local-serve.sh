8ca20af226cc01d0c95e599bedaaeffae18cfabd
Ivan Dachev <i_dachev@yahoo.co.uk>
2024-09-01 08:53:30 +0300

    Updated

--- utils/local-serve.sh
+++ utils/local-serve.sh
@@ -11,8 +11,6 @@ BUILD_VERSION=$(date -u '+%Y-%m-%d')-$(git rev-parse --short HEAD)
 rm ./_config-build.yml || true
 echo -e "build_version: ${BUILD_VERSION}" > ./_config-build.yml
 
-JEKYLL_ENV=production bundle exec jekyll build \
+JEKYLL_ENV=production bundle exec jekyll serve \
   --config _config.yml,_config-build.yml
 
-echo -e "\n\nBuild complete:\n"
-ls -al ./_site
